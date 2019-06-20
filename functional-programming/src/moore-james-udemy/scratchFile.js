26647de98d152596e3b8c0c61b8520b91a599807
Gourav B <pit.wire@pm.me>
2019-06-20 07:56:38 -0400

    updating object properties in an immutable way

--- functional-programming/src/moore-james-udemy/scratchFile.js
+++ functional-programming/src/moore-james-udemy/scratchFile.js
@@ -34,3 +34,32 @@ console.log(description, calories);
 const {id, ...mealWithoutId } = updatedMeal;
 
 console.log(mealWithoutId);
+
+////////////////////////////////////////////
+
+const meal = {
+    description: 'Dinner',
+};
+// 1. In an Immutable way, add a property to the
+// meal called calories setting it's value to 200,
+// then log the result to the console
+const updatedMeal = {
+    ...meal,
+    calories: 200
+};
+
+console.log(updatedMeal);
+
+// 2. In an Immutable way, increase the calories
+// by 100 and print the result to the console
+const updatedMeal2 = {
+    ...updatedMeal,
+    calories: updatedMeal.calories + 100
+};
+console.log(updatedMeal2);
+
+// 3. In an Immutable way, remove the calories property and log the result to the console
+const {calories, ...everythingButCals} = updatedMeal2;
+console.log(everythingButCals);
+
+///////////////////////////////////////////////////////
