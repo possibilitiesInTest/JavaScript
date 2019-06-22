a5afdc68a49735bd905832a3961bd6e5f15e173b
Gourav B <pit.wire@pm.me>
2019-06-21 23:42:27 -0400

    using reduce to summarize/extract info from arrays

--- functional-programming/src/moore-james-udemy/scratchFile.js
+++ functional-programming/src/moore-james-udemy/scratchFile.js
@@ -139,3 +139,46 @@ const shorterNamedFriends = updatedFriends.filter(
 // 5. Print each variable to the console.
 console.log(friends, updatedFriends, friendNameLengths, shorterNamedFriends);
 
+/////////////////////////////////////////////////
+// using reduce to extract info from arrays
+
+const grades  = [60, 55, 80, 90, 99, 92, 75, 72];
+
+const total = grades.reduce(sum);
+
+function sum(acc, grade) {
+    return acc + grade;
+}
+
+const count = grades.length;
+
+const letterGradeCount = grades.reduce(groupByGrade, {});
+
+function groupByGrade(acc, grade){
+    const {a = 0, b = 0, c = 0, d = 0, f = 0 } = acc;
+    if (grade >= 90) {
+        return {...acc, a: a + 1};
+    } else if (grade >= 80) {
+        return {...acc, b: b + 1};
+    } else if (grade >= 70) {
+        return {...acc, c: c + 1};
+    } else if (grade >= 60) {
+        return {...acc, d: d + 1};
+    } else {
+        return { ...acc, f: f + 1};
+    }
+}
+
+console.log(total, total / count, letterGradeCount);
+
+
+// 623
+// 77.875
+//     [object Object] {
+//     a: 3,
+//         b: 1,
+//         c: 2,
+//         d: 1,
+//         f: 1
+// }
+
