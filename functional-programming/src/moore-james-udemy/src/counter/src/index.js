0627d860e3caeddddcfc95a2caba35d94713de1b
gourav b <pit.wire@pm.me>
2019-07-30 14:26:49 -0400

    implment virtual dom to conditionally render app components:
    in app()
         -aft currentView: set rootNode to res createElm passing in currentView
         -pass rootNode to appendChild
    
         in dispatch()
           -aft updtdVw: set patches to diff btwn current & updated views
           -set rootNode to res of patch, applying patches to rootNode
           -rm: replaceChild call on node

--- functional-programming/src/moore-james-udemy/src/counter/src/index.js
+++ functional-programming/src/moore-james-udemy/src/counter/src/index.js
@@ -1,5 +1,6 @@
-import h from 'hyperscript';
 import hh from 'hyperscript-helpers';
+import { h, diff, patch } from 'virtual-dom';
+import createElement from 'virtual-dom/create-element';
 
 const { div, button } = hh(h);
 
@@ -34,15 +35,19 @@ function update(msg, model) {
 // impure code below
 
 function app(initModel, update, view, node){
+
+    // sets up initial app state
     let model = initModel;
     let currentView = view(dispatch, model);
-    node.appendChild(currentView);
+    let rootNode = createElement(currentView);
+    node.appendChild(rootNode);
 
     // handles update sequence on app interaction
     function dispatch(msg){
         model = update(msg, model);
         const updatedView = view(dispatch, model);
-        node.replaceChild(updatedView, currentView);
+        const patches = diff(currentView, updatedView);
+        rootNode = patch(rootNode, patches);
         currentView = updatedView;
     }
 }
