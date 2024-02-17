bc1b664619eaf6b9c5d180e3a85e7aa87f7f5e43
possibilitiesInTest <possibilitiesInTest@gmail.com>
2024-02-17 11:43:48 -0500

    double equals type converts second var to first

---
+++ Traub-Eric-JS-Practice/8_equals_vs_strict.js
@@ -0,0 +1,15 @@
+console.log(7 == '7');  // true
+ 
+console.log(7 === '7');  // false
+
+
+/*
+==
+
+num == str // string converted to num
+
+boolean == non-boolean // non-boolean converted to boolean
+
+object == primitive // object converted to primitive
+
+*/
