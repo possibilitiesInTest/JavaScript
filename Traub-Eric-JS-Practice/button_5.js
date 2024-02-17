ffff49aa579fb4868a832e733c3d2f489f0e4f55
possibilitiesInTest <possibilitiesInTest@gmail.com>
2024-02-17 02:16:06 -0500

    button_5 external addClick solution, and let solution: block scoped variable, not function scoped

--- Traub-Eric-JS-Practice/button_5.js
+++ Traub-Eric-JS-Practice/button_5.js
@@ -12,17 +12,43 @@
   
 //  createButtons();
 
+  // Solution 2:
 
- function createButtons() {
+function createButtons() {
     for (var i = 1; i <= 5; i++) {
       var body = document.getElementsByTagName("BODY")[0];
       var button = document.createElement("BUTTON");
       button.innerHTML = 'Button ' + i;
-      (function(num) {
+ //      (function(num) {
+ //      button.onclick = function() {
+ //           alert('This is button ' + num);
+ //      };
+ //      })(i)
+      addClickFunctionality(button, i);
+      body.appendChild(button);
+    }
+ }
+  
+ createButtons();
+ 
+ 
+ function addClickFunctionality(button, num) {
+     button.onclick = function() {
+         alert('This is button ' + num);
+     }
+  }
+
+  // Solution 3:
+  //   let is block scoped instead of function scoped
+ 
+function createButtons() {
+    for (let i = 1; i <= 5; i++) {
+      var body = document.getElementsByTagName("BODY")[0];
+      var button = document.createElement("BUTTON");
+      button.innerHTML = 'Button ' + i;
       button.onclick = function() {
-           alert('This is button ' + num);
-      };
-      })(i)
+           alert('This is button ' + i);
+      }
       body.appendChild(button);
     }
  }
