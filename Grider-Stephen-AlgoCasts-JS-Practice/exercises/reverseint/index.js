f1ac1b4490a3516c33366bcaf35b4e941ca755a5
possibilitiesInTest <possibilitiesInTest@gmail.com>
2024-02-18 21:59:05 -0500

    reverseInt solution:
         -convert int toString then reverse.split.join
         -multiply parsedInt of result by math.sign of n

--- Grider-Stephen-AlgoCasts-JS-Practice/exercises/reverseint/index.js
+++ Grider-Stephen-AlgoCasts-JS-Practice/exercises/reverseint/index.js
@@ -8,6 +8,10 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    n.toString().split('').reverse().join();
+
+    return Math.sign(n) * parseInt(n);
+}
 
 module.exports = reverseInt;
