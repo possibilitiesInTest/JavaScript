64e8ce6fb32a25457bc91760699376c6b45cfc96
possibilitiesInTest <possibilitiesInTest@gmail.com>
2024-02-20 23:08:18 -0500

    class inheritance w. polymorphism example

--- Traversy_Brad_Modern_JS/script.js
+++ Traversy_Brad_Modern_JS/script.js
@@ -1,37 +1,110 @@
-function Player(name) {
-    this.name = name;
-    this.lvl = 1;
-    this.points = 0;
-}
+// Parent class
+class Shape {
+    constructor(name) {
+        this.name = name;
+    }
 
-Player.prototype.gainXp = function (xp) {
-    this.points += xp;
-
-    if(this.points >= 10) 
-    {   this.lvl++;
-        this.points -= 10;
+    logName() {
+        console.log('Shape name: ', this.name);
     }
 }
 
-Player.prototype.describe = function(){
-    return `${this.name} is level ${this.lvl} with ${this.points}
-    experience points`;
+// Sub Class
+class Rectangle extends Shape {
+    constructor(name, width, height) {
+        super(name);
+
+        this.height = height;
+        this.width = width;
+    }
 }
 
-const player1 = new Player('Bob');
-const player2 = new Player('Alice');
+class Circle extends Shape {
+    constructor(name, radius) {
+        super(name);
 
-player1.gainXp(4);
-player2.gainXp(7);
-player1.gainXp(5);
-player2.gainXp(1);
-player1.gainXp(7);
-player2.gainXp(9);
-player1.gainXp(5);
-player2.gainXp(2);
+        this.readius = radius;
+    }
 
-console.log(player1.describe());
-console.log(player2.describe());
+    logName() {
+        console.log('Circle Name: ' + this.name)
+    }
+}
+
+
+
+
+const rect = new Rectangle('Rect 1', 20, 20);
+console.log(rect);
+rect.logName();
+
+const cir = new Circle('Cir 1', 30);
+cir.logName();
+
+console.log(rect instanceof Rectangle);
+console.log(rect instanceof Shape);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Player(name) {
+//     this.name = name;
+//     this.lvl = 1;
+//     this.points = 0;
+// }
+
+// Player.prototype.gainXp = function (xp) {
+//     this.points += xp;
+
+//     if(this.points >= 10) 
+//     {   this.lvl++;
+//         this.points -= 10;
+//     }
+// }
+
+// Player.prototype.describe = function(){
+//     return `${this.name} is level ${this.lvl} with ${this.points}
+//     experience points`;
+// }
+
+// const player1 = new Player('Bob');
+// const player2 = new Player('Alice');
+
+// player1.gainXp(4);
+// player2.gainXp(7);
+// player1.gainXp(5);
+// player2.gainXp(1);
+// player1.gainXp(7);
+// player2.gainXp(9);
+// player1.gainXp(5);
+// player2.gainXp(2);
+
+// console.log(player1.describe());
+// console.log(player2.describe());
 
 
 
