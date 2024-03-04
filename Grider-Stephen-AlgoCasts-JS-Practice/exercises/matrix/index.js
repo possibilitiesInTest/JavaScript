a07c696d5d4c22f9f818dab15456f96866670bc3
possibilitiesInTest <possibilitiesInTest@gmail.com>
2024-03-04 15:32:45 -0500

    matrix solution:
         -set results[], counter, start col|row to zero
          set end col|row to n-1
    
         -iterate i to less than n
            push to results as []
    
         -while start_col <= end_col and start_row <= end_row
    
         //top row
         -iterate i at start_col <= end_col
         -set start_row at i to counter
          increment counter
    
         -increment start_row
    
         //Right_col
         -iterate i at start_row <= end_row
         -set results at i at end_col to counter
          increment counter
    
         -decrement end_col
    
         //Bottom Row
         iterate i at end_col >= start_col decrement i
         -set results at end_row at i to counter
         -increment counter
    
         -decrement end_row
    
         //start col
         -iterate i at end_row >= start_row decrement i
         -set results at i at start_col to counter
          increment counter
    
         =increment start_col
    
         -return results

--- Grider-Stephen-AlgoCasts-JS-Practice/exercises/matrix/index.js
+++ Grider-Stephen-AlgoCasts-JS-Practice/exercises/matrix/index.js
@@ -15,6 +15,50 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    const results = [];
+    let counter = 1;
+    
+    for (let i = 0; i < n; i++) {
+        results.push([]);
+      }
+
+    let start_col = 0;
+    let start_row = 0;
+    let end_col = n - 1;
+    let end_row = n - 1;
+
+    while(start_col <= end_col && start_row <= end_row) {
+
+        // Top Row
+        for(let i = start_col; i <= end_col; i++){
+            results[start_row][i] = counter;
+            counter++;
+        }
+        start_row++;
+
+        // Right Column
+        for(let i = start_row; i <= end_row; i++){
+            results[i][end_col] = counter;
+            counter++;
+        }
+        end_col--;
+
+        // Bottom Row
+        for(let i = end_col; i >= start_col; i--){
+            results[end_row][i] = counter;
+            counter++;
+        }
+        end_row--;
+
+        // Start Column
+        for(let i = end_row; i >= start_row; i--) {
+            results[i][start_col] = counter;
+            counter++;
+        }
+        start_col++;
+    }
+    return results;
+}
 
 module.exports = matrix;
