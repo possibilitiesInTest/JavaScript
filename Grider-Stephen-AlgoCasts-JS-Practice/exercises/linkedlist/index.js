dccae0ea88341828239c6fce855bb85e7deaf311
possibilitiesInTest <possibilitiesInTest@gmail.com>
2024-03-07 19:03:29 -0500

    linkedList:
         clear()
              set head to null
    
         removeFirst()
              if head does not exist: return null
              set head to next
    
         removeLast()
               if head does not exist: return
               if the next property of head does not exist
                    set head to null, return
               set previous to head, set node to next
               while node.next exists
                     set previous to node, node to next
               set the next of previous to null
    
         insertLast()
               set last to getLast()
               if last exists
                     set next of last to new Node(data)
               else: set head to new Node(data)

--- Grider-Stephen-AlgoCasts-JS-Practice/exercises/linkedlist/index.js
+++ Grider-Stephen-AlgoCasts-JS-Practice/exercises/linkedlist/index.js
@@ -39,14 +39,56 @@ class LinkedList {
 
         let node = this.head;
         while(node){
-            if (node.next === null){
+            if (!node.next){
                 return node;
             }
-            node = node.next;
-          
+            node = node.next;          
         }
     }
 
+    clear() {
+        this.head = null;
+    }
+
+    removeFirst() {
+        if(!this.head){
+            return null;
+        }
+
+        this.head = this.head.next;
+    }
+
+    removeLast(){
+        // for empty list
+        if(!this.head){
+            return;
+        }
+
+        // for list length one
+        if(!this.head.next){
+            this.head = null;
+            return;
+        }
+
+        // for list longer than one
+        let previous = this.head;
+        let node = this.head.next;
+        while(node.next){
+            previous = node;
+            node = node.next
+        }
+        previous.next = null;
+    }
+
+    insertLast(data) {
+        const last = this.getLast();
+
+        if(last) {
+            last.next = new Node(data);
+        } else {
+            this.head = new Node(data);
+        }
+    }
 }
 
 module.exports = { Node, LinkedList };
