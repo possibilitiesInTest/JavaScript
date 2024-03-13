194aee61c5b764a81c11d65cf90dc11ac9983bd5
possibilitiesInTest <possibilitiesInTest@gmail.com>
2024-03-12 22:19:05 -0400

    bst:
         class Node
               constructor: pass in data
                   this.data as data
                   this.right as null
                   this.left as null
    
               insert: pass in data
                   handle less than current node w. left child
                        this.left.insert(data)
                   handles less than current node w.o left
                        this.left = new Node(data)
    
                   handle greater than current node w. right child
                        this.right.insert(data)
                   handle greater than current node w.o right
                        this.right = new Node(data)
    
                contains: pass in data
                       if(this.data equals data): return this
    
                       if(this.data < data && this.right)
                            return this.right.contains(data)
                       else this.d greater than data and this.left
                            return this.left.contains(data)
                       return null

--- Grider-Stephen-AlgoCasts-JS-Practice/exercises/bst/index.js
+++ Grider-Stephen-AlgoCasts-JS-Practice/exercises/bst/index.js
@@ -11,6 +11,42 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    insert(data){
+        // handle less than current node w. left child
+        if(data < this.data && this.left){
+            this.left.insert(data)
+        // handle less than current node w/o left
+        } else if (data < this.data) {
+            this.left = new Node(data)
+
+        // handle greater than current node w. right child
+        }else if(data > this.data && this.right) {
+                this.right.insert(data);
+        // handle greater than current node w/o right
+            } else if(data > this.data){
+                this.right = new Node(data)
+            }
+        }
+
+    contains(data){
+        if(this.data === data){
+            return this;
+        }
+
+        if (this.data < data && this.right){
+            return this.right.contains(data);
+        } else if (this.data > data && this.left){
+            return this.left.contains(data)
+        }
+        return null;
+        }
+}
 
 module.exports = Node;
