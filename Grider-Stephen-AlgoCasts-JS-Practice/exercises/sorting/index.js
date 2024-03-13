19e983e3e1fcb26e6df739f8001e71ba978dd879
possibilitiesInTest <possibilitiesInTest@gmail.com>
2024-03-13 10:17:27 -0400

    selectionSort
         iterate through arr as i
            set indexOfMin to i
    
            iterate through arr as j starting at i plus one
                if elm at j is less than elm at indexOfMin
                   set ndexOfMin to j
    
          if indexOfMin d.equal i
             swap arr at indexOfMin and I
             set temp to arr at indexOfMin
             set arr at indexOfMin to arr at i
             set arr at i to temp
    
          return arr

--- Grider-Stephen-AlgoCasts-JS-Practice/exercises/sorting/index.js
+++ Grider-Stephen-AlgoCasts-JS-Practice/exercises/sorting/index.js
@@ -7,9 +7,9 @@ function bubbleSort(arr) {
         for(let j =0; j < arr.length - i - 1; j++){
  
             if(arr[j] > arr[j+1]){
-                const lesser = arr[j+1];
+                const temp = arr[j+1];
                 arr[j + 1] = arr[j];
-                arr[j] = lesser;
+                arr[j] = temp;
             }
         }
     }
@@ -18,6 +18,22 @@ function bubbleSort(arr) {
 
 function selectionSort(arr) {
 
+    for (let i = 0; i < arr.length; i++){
+        let indexOfMin = i;
+
+        for(let j = i + 1; j < arr.length; j++){
+            if(arr[j] < arr[indexOfMin]){
+                indexOfMin = j;
+            }
+        }
+
+    if(indexOfMin !== i) {
+        let temp = arr[indexOfMin];
+        arr[indexOfMin] = arr[i];
+        arr[i] = temp;
+    }
+    }
+    return arr;
 }
 
 function mergeSort(arr) {
